16fed83901960d6e9afcb89fd2fbc4934638a33a
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-03 17:45:26 +0100

    correcttion

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import User from './User'
 
 export default function ListUser({ users }) {
+
+    const [activeId, setActiveId] = useState(0);
+
+    const changeId = (id) => {
+        setActiveId(id);
+    }
+
     return (
         <div className='list'>
+            <h1>Nombre d'utilisateurs : {users.length}</h1>
             {users.map(u =>
-                <User key={u.id} user={u}/>
+                <User key={u.id} user={u} active={activeId} handleClick={changeId}/>
             )}
         </div>
     )
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-export default function User({user}) {
-  return (
-    <div className='user'>
-        <h2>Name : {user.name}</h2>
-        <p>Email : {user.email}</p>
-        <p>Ville : {user.address.city} - Rue : {user.address.street}</p>
-        <button>Details user</button>
-    </div>
-  )
+export default function User({ user, active, handleClick }) {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        if (user.id === active) {
+            const getPosts = async () => {
+                const res = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=' + active);
+                return res.data;
+            }
+
+            getPosts().then(posts => setPosts(posts))
+        }
+    }, [active])
+    return (
+        <div className='user'>
+            <h2>Name : {user.name}</h2>
+            <p>Email : {user.email}</p>
+            <p>Ville : {user.address.city} - Rue : {user.address.street}</p>
+            <button onClick={() => handleClick(user.id)} disabled={active === user.id}>Details user</button>
+
+            {
+                user.id === active && <div className='posts'>
+                    <h4>nombre de posts : {posts.length}</h4>
+                    {
+                        posts.map(p =>
+                            <div className='post'>
+                                <h5>{p.title}</h5>
+                                <p>{p.body}</p>
+                            </div>
+                        )
+                    }
+                </div>
+            }
+        </div>
+    )
 }
